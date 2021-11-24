c8d1305c4d07536e64782ba091ce73eece20f274
Marius Amza <mariusamza@gmail.com>
2021-11-24 20:36:16 +0200

    Initializare header

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Plugin Colaborare
+Plugin URI: https://swissacademy.eu
+Description: This is a plugin that creates a WordPress Widget.
+Author: Marius Amza
+Version: 0.1
+Author URI: https://swissacademy.eu
+*/
