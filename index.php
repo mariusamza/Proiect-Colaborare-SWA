70fd717d0837f3e88a70bb5ec1cd05e9c36a6976
SorinSI9 <stama.sorin@yahoo.com>
2021-11-24 20:37:22 +0200

    Modificare Autor

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Plugin Colaborare
 Plugin URI: https://swissacademy.eu
 Description: This is a plugin that creates a WordPress Widget.
-Author: Marius Amza
-Version: 0.1
+Author: Marius Amza/ Testare
+Version: 0.2
 Author URI: https://swissacademy.eu
 */
